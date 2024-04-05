c5e7fab8193fa48fe822066146851ac139f51c9e
jcl <chaelynjang@lotte.net>
2024-04-05 17:57:51 +0900

    PathVariable api 추가

--- src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.myrestfulservice.controller;
 
 import com.example.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         // 스프링 부트가 json 형태로 리턴
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        // 스프링 부트가 json 형태로 리턴
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
