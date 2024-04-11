0889e04a9a51cdf28f879428df5b75526abdc1c7
jcl <chaelynjang@lotte.net>
2024-04-11 10:01:01 +0900

    post 요청 처리 시, 헤더에 클라이언트가 다음에 요청해볼 수 있는 location uri(생성된 user 상세보기 등) 담아서 리턴

--- src/main/java/com/example/myrestfulservice/controller/UserController.java
+++ src/main/java/com/example/myrestfulservice/controller/UserController.java
@@ -3,10 +3,11 @@ package com.example.myrestfulservice.controller;
 import com.example.myrestfulservice.bean.User;
 import com.example.myrestfulservice.dao.UserDaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,17 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return userService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        // response 헤더에 key: location으로 생성된 user 상세보기 uri 를 담아서 보낼 수 있음
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        // http status code: 201
+        return ResponseEntity.created(location).build();
     }
 }
