23c56c900e5a52484b5af85046d0906eb4086526
jcl <chaelynjang@lotte.net>
2024-04-18 10:44:44 +0900

    ExceptoinHandler 통한 예외 핸들링

---
+++ src/main/java/com/example/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.myrestfulservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), e.getMessage()
+                , request.getDescription(false)); // 클라이언트에게 상세정보 안보여주도록 false
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), e.getMessage()
+                , request.getDescription(false)); // 클라이언트에게 상세정보 안보여주도록 false
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
