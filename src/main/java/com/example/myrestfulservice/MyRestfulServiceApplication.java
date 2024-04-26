a813e559d2ed978e7ab58dd3c91c5d8873f8c091
jcl <chaelynjang@lotte.net>
2024-04-26 17:27:19 +0900

    다국어 처리 구현

--- src/main/java/com/example/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/com/example/myrestfulservice/MyRestfulServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -16,4 +21,10 @@ public class MyRestfulServiceApplication {
 //		}
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.example.myrestfulservice.controller;
 
 import com.example.myrestfulservice.bean.HelloWorldBean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
+    private final MessageSource messageSource;
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -23,4 +31,10 @@ public class HelloWorldController {
         // 스프링 부트가 json 형태로 리턴
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping("/hello-world-i18n")
+    public String helloWorldBeanI18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
 }
