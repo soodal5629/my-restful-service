7476a75dcb82ec84d0c1200cb4f7c0896485ca69
jcl <chaelynjang@lotte.net>
2024-05-09 17:12:31 +0900

    jpa 리소스 변경 시 +security 설정 츠기

--- src/main/java/com/example/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/com/example/myrestfulservice/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.example.myrestfulservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
@@ -33,5 +37,14 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")); // h2-console 요청은 인증 x
+    }
+
+    // 얘를 빈등록 하지 않으면 리소스에 변화가 일어나는 delete/update 등의 요청이 올 때 401 에러가 발생함
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           HandlerMappingIntrospector introspector) throws Exception {
+        // csrf
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
--- src/main/java/com/example/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/com/example/myrestfulservice/controller/UserJpaController.java
@@ -7,12 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -25,8 +28,12 @@ public class UserJpaController {
     private final UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers() {
-        return userRepository.findAll();
+    public ResponseEntity retrieveAllUsers() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("count", userRepository.count());
+        map.put("users", userRepository.findAll());
+
+        return ResponseEntity.ok(map);
     }
 
     @GetMapping("/users/{id}")
@@ -39,6 +46,25 @@ public class UserJpaController {
         WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         entityModel.add(linTo.withRel("all-users"));
         return ResponseEntity.ok(entityModel);
+    }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Validated @RequestBody User user) {
+        if (user.getJoinDate() == null) {
+            user.setJoinDate(LocalDateTime.now());
+        }
+        User savedUser = userRepository.save(user);
+        // response 헤더에 key: location으로 생성된 user 상세보기 uri 를 담아서 보낼 수 있음
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        // http status code: 201
+        return ResponseEntity.created(location).build();
     }
 }
